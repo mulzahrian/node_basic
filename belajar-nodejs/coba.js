f12989b94ed2042ed4997b909ccbb9f7a3d2a4ad
mulzahrian <mulzahrianone@gmail.com>
2024-10-01 21:12:09 +0700

    1.Add learning module how to run node js for the first time

--- belajar-nodejs/coba.js
+++ belajar-nodejs/coba.js
@@ -1,1 +1,7 @@
 console.log("hellow world");
+
+function cetakNama(nama) {
+  return `Halo,Nama saya adalah ${nama}`;
+}
+
+module.exports = cetakNama;
--- belajar-nodejs/index.js
+++ belajar-nodejs/index.js
@@ -3,4 +3,9 @@
 // const cetakNama = (nama) => `Hi, Nama saya ${nama}`;
 // console.log(cetakNama(nama));
 
+//cara menjalankan file nya bisa dengan coba node index.js
+
+const cetakNama = require("./coba");
 console.log("hellow Ari");
+
+cetakNama("Ari");

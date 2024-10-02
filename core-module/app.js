54d04762f08408e8bcdb5b0284fc89bc1f677285
mulzahrian <mulzahrianone@gmail.com>
2024-10-02 23:39:24 +0700

    1.Add core module 2

--- core-module/app.js
+++ core-module/app.js
@@ -23,7 +23,44 @@ const fs = require("fs");
 // console.log(cetak);
 
 //membaca file asyn
-fs.readFile("data/test.txt", "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
+// fs.readFile("data/test.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//Readline
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("siapa nama kamu? ", (answer) => {
+  rl.question("dimana kamu tinggal? ", (live) => {
+    // TODO: Log the answer in a database
+    console.log(`nama kamu adalah : ${answer}`, `kamu tinggal di : ${live}`);
+    const contact = {
+      answer,
+      live,
+    };
+
+    const fileBufer = fs.readFileSync("data/contact.json", "utf-8");
+    //console.log(fileBufer);
+    const data = JSON.parse(fileBufer);
+    data.push(contact);
+    console.log(data);
+
+    fs.writeFileSync("data/contact.json", JSON.stringify(data));
+
+    // fs.writeFile(
+    //   "data/profile.txt",
+    //   `nama kamu adalah : ${answer} kamu tinggal di : ${live}`,
+    //   (err) => {
+    //     console.log("ini adalah error " + err);
+    //   }
+    // );
+    console.log("data has created/updated in profile.txt");
+    rl.close();
+  });
 });

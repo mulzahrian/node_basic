da3817108b602a2dc2360754a62771c10005f8eb
mulzahrian <mulzahrianone@gmail.com>
2024-10-04 22:27:46 +0700

    1.Add new changes module npm

--- npm-module/app.js
+++ npm-module/app.js
@@ -1,1 +1,4 @@
-console.log("ari ganteng");
+const validator = require("validator");
+
+console.log(validator.isEmail("mulzahrianone@gmail.com"));
+console.log(validator.isMobilePhone("085267522837", "id-ID"));

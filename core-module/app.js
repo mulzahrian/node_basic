2993271f95d0b8f6ea25a7a998668f37209ba599
mulzahrian <mulzahrianone@gmail.com>
2024-10-05 23:12:41 +0700

    1.add new changes data for node core

--- core-module/app.js
+++ core-module/app.js
@@ -36,31 +36,63 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("siapa nama kamu? ", (answer) => {
-  rl.question("dimana kamu tinggal? ", (live) => {
-    // TODO: Log the answer in a database
-    console.log(`nama kamu adalah : ${answer}`, `kamu tinggal di : ${live}`);
-    const contact = {
-      answer,
-      live,
-    };
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
 
-    const fileBufer = fs.readFileSync("data/contact.json", "utf-8");
-    //console.log(fileBufer);
-    const data = JSON.parse(fileBufer);
-    data.push(contact);
-    console.log(data);
+//membuat file contact.json
+const dataPath = "./data/contact.json";
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, "[]", "utf-8");
+}
 
-    fs.writeFileSync("data/contact.json", JSON.stringify(data));
-
-    // fs.writeFile(
-    //   "data/profile.txt",
-    //   `nama kamu adalah : ${answer} kamu tinggal di : ${live}`,
-    //   (err) => {
-    //     console.log("ini adalah error " + err);
-    //   }
-    // );
-    console.log("data has created/updated in profile.txt");
-    rl.close();
+const pertanyaan1 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question("Masukkan Nama anda : ", (nama) => {
+      resolve(nama);
+    });
   });
-});
+};
+
+const pertanyaan2 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question("Masukkan Email anda : ", (email) => {
+      resolve(email);
+    });
+  });
+};
+
+const main = async () => {
+  const nama = await pertanyaan1();
+  const email = await pertanyaan2();
+};
+
+// rl.question("siapa nama kamu? ", (answer) => {
+//   rl.question("dimana kamu tinggal? ", (live) => {
+//     // TODO: Log the answer in a database
+//     console.log(`nama kamu adalah : ${answer}`, `kamu tinggal di : ${live}`);
+//     const contact = {
+//       answer,
+//       live,
+//     };
+
+//     const fileBufer = fs.readFileSync("data/contact.json", "utf-8");
+//     //console.log(fileBufer);
+//     const data = JSON.parse(fileBufer);
+//     data.push(contact);
+//     console.log(data);
+
+//     fs.writeFileSync("data/contact.json", JSON.stringify(data));
+
+//     // fs.writeFile(
+//     //   "data/profile.txt",
+//     //   `nama kamu adalah : ${answer} kamu tinggal di : ${live}`,
+//     //   (err) => {
+//     //     console.log("ini adalah error " + err);
+//     //   }
+//     // );
+//     console.log("data has created/updated in profile.txt");
+//     rl.close();
+//   });
+// });

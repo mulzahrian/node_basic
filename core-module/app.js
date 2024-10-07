36d0e5dbbea712a003b42f3012236eae191c5f28
mulzahrian <mulzahrianone@gmail.com>
2024-10-07 22:15:02 +0700

    1.add yargs "but still error"

--- core-module/app.js
+++ core-module/app.js
@@ -1,12 +1,27 @@
 const yargs = require("yargs");
 
-yargs.command(
-  "add",
-  "Menambahkan contact baru",
-  () => {},
-  (argv) => {
-    console.log(argv.nama);
-  }
-);
+yargs.command({
+  conmmand: "add",
+  describe: "add new contact",
+  builder: {
+    nama: {
+      describe: "Full Name",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    const contact = {
+      nama: argv.nama,
+      email: argv.email,
+    };
+    console.log(contact);
+  },
+});
 
 yargs.parse();

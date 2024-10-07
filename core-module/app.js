4142998fde7495bf3b590270fa1ecb8cc6dbe9eb
mulzahrian <mulzahrianone@gmail.com>
2024-10-07 21:52:45 +0700

    1.Made add data using yargs

--- core-module/app.js
+++ core-module/app.js
@@ -1,23 +1,12 @@
 const yargs = require("yargs");
 
-//Mengambil argument dari command line
-//console.log(process.argv);
-//console.log(process.argv[2]);
-// const command = process.argv[2];
-// if (command === "add") {
-// } else if (command === "remove") {
-// } else if (command === "list") {
-// }
+yargs.command(
+  "add",
+  "Menambahkan contact baru",
+  () => {},
+  (argv) => {
+    console.log(argv.nama);
+  }
+);
 
-console.log(yargs.argv);
-
-// const { writeQustion, saveContact } = require("./contacts");
-
-// const main = async () => {
-//   const nama = await writeQustion("Masukkan Nama Anda : ");
-//   const email = await writeQustion("Masukkan Email Anda : ");
-
-//   saveContact(nama, email);
-// };
-
-// main();
+yargs.parse();
